8e2286e40e137250bf004025e06ca24d79bf529b
mp607 <mickey60711@gmail.com>
2012-07-04 13:44:14 +0800

    add Level Button. add PlayGame View.

---
+++ GoldMining/GoldMiningPlayGame.m
@@ -0,0 +1,43 @@
+//
+//  GoldMiningPlayGame.m
+//  GoldMining
+//
+//  Created by 機器 王 on 12/7/4.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "GoldMiningPlayGame.h"
+
+@interface GoldMiningPlayGame ()
+
+@end
+
+@implementation GoldMiningPlayGame
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
