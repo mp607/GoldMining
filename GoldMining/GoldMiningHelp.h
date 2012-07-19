03e2dc359528a9139ad5312ddf8125265f00d7fb
mp607 <mickey60711@gmail.com>
2012-07-19 14:39:00 +0800

    add helpView Class and help.html

---
+++ GoldMining/GoldMiningHelp.h
@@ -0,0 +1,13 @@
+//
+//  GoldMiningHelp.h
+//  GoldMining
+//
+//  Created by 王 機器 on 12/7/19.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GoldMiningHelp : UIViewController
+
+@end
---
+++ GoldMining/GoldMiningHelp.m
@@ -0,0 +1,67 @@
+//
+//  GoldMiningHelp.m
+//  GoldMining
+//
+//  Created by 王 機器 on 12/7/19.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "GoldMiningHelp.h"
+
+@interface GoldMiningHelp ()
+
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@end
+
+@implementation GoldMiningHelp
+@synthesize webView;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{	
+	// 取得檔案路徑 
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"help" ofType:@"html"];
+
+	
+	if ( [[NSFileManager defaultManager] fileExistsAtPath:path] )
+	{
+	
+		//htmlString = [NSString stringWithContentsOfFile:htmlFile encoding:NSUTF8StringEncoding error:nil];
+		[webView loadRequest:[NSURLRequest requestWithURL:[NSURL fileURLWithPath:path]]];
+	}
+	else {
+		NSString *htmlString = @"<H1>404 Not Found</H1>";
+		[webView loadHTMLString:htmlString baseURL:nil];
+	}
+	
+	[super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+	[self setWebView:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (IBAction)homeBtnPressed:(id)sender 
+{
+	// bake to Home
+	[self dismissModalViewControllerAnimated:YES];
+}
+
+@end
